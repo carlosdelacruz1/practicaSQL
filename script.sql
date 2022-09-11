e8148b209e6b4958d5207e544b2667ca506c1fac
Carlos dCP <37401820+carlosdelacruz1@users.noreply.github.com>
2022-09-12 01:40:34 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -47,6 +47,7 @@ add constraint modelos_PK primary key(idmodelo);
 
 create table fecha_compra(
 	fecha date not null, --PK
+	name varchar(20) not null,
 	comentario varchar(512) null
 
 );
@@ -57,8 +58,8 @@ add constraint fecha_compra_PK primary key(fecha);
 -- tabla matricula
 
 create table matricula(
-	matricula_num varchar(10) not null, --PK
-	name varchar(10) not null,
+	matricula_num varchar(20) not null, --PK
+	name varchar(20) not null,
 	comentario varchar(512) null
 
 );
@@ -81,7 +82,7 @@ add constraint colores_PK primary key(color);
 
 create table kilometraje_total(
 	kilometraje integer not null, --PK
-	name integer not null,
+	name varchar(20) not null,
 	comentario varchar(512) null
 
 );
@@ -93,7 +94,9 @@ add constraint kilometraje_total_PK primary key(kilometraje);
 
 create table aseguradoras(
 	id_aseguradoras varchar(20) not null, --PK
-	name varchar(20) not null
+	name varchar(20) not null,
+	comentario varchar(512) null
+
 );
 
 alter table aseguradoras
@@ -102,8 +105,10 @@ add constraint aseguradoras_PK primary key(id_aseguradoras);
 -- tabla numero de poliza
 
 create table polizas(
-	num_poliza varchar(10) not null, --PK
-	name varchar(10) not null
+	num_poliza varchar(20) not null, --PK
+	name varchar(20) not null,
+	comentario varchar(512) null
+	
 	);
 
 alter table polizas
@@ -112,18 +117,22 @@ add constraint polizas_PK primary key(num_poliza);
 -- tabla numero de revisiones
 
 create table revisiones(
-	revisiones_num integer not null, --PK
-	name varchar(10) not null
+	num_revision integer not null, --PK
+	name varchar(20) not null,
+	comentario varchar(512) null
+
 	);
 
 alter table revisiones
-add constraint revisiones_PK primary key(revisiones_num);
+add constraint revisiones_PK primary key(num_revision);
 
 -- tabla kilometros revision
 
 create table kilometros_revisiones(
 	kilometros_revision integer not null, --PK
-	name varchar(10) not null
+	name varchar(20) not null,
+	comentario varchar(512) null
+
 	);
 
 alter table kilometros_revisiones
@@ -134,7 +143,9 @@ add constraint kilometros_revisiones_PK primary key(kilometros_revision);
 
 create table fechas_revision(
 	fecha_revision date not null, --PK
-	name varchar(10) not null
+	name varchar(20) not null,
+	comentario varchar(512) null
+
 	);
 
 alter table fechas_revision
@@ -143,8 +154,10 @@ add constraint fechas_revision_PK primary key(fecha_revision);
 -- tabla importe revision
 
 create table importe_revisiones(
-	precio_revision money  not null, --PK
-	name varchar(10) not null
+	precio_revision money not null, --PK
+	name varchar(20) not null,
+	comentario varchar(512) null
+
 	);
 
 alter table importe_revisiones
@@ -154,22 +167,22 @@ add constraint importe_revisiones_PK primary key(precio_revision);
 --- Creamos los coches
 
 create table coches(
-	idcoche varchar(10)  not null, --PK
-	name varchar(10) not null, 
+	idcoche varchar(20)  not null, --PK
+	name varchar(20) null, 
 	idgrupo varchar(20) not null, --FK
 	idmarca varchar(20) not null, --FK
 	idmodelo varchar(20) not null, --FK
-	matricula_num varchar(10) not null, --FK
+	matricula_num varchar(20) not null, --FK
 	color varchar(20) not null, --FK
 	kilometraje integer not null, --FK
 	id_aseguradoras varchar(20) not null, --FK
-	num_poliza varchar(10) not null, --FK
-	revisiones_num integer not null, --FK
+	num_poliza varchar(20) not null, --FK
+	num_revision integer not null, --FK
 	kilometros_revision integer not null, --FK
 	fecha date not null, --FK
 	fecha_revision date not null, --FK
 	precio_revision money  not null, --FK
-	comentario varcahr(512) null
+	comentario varchar(512) null
 
 	);
 
@@ -177,7 +190,7 @@ alter table coches
 add constraint coches_PK primary key(idcoche);
 
 alter table coches
-add constraint coches_grupo_FK foreign key(idgrupo) references grupo_emrpesarial(idgrupo);
+add constraint coches_grupo_FK foreign key(idgrupo) references grupo_empresarial(idgrupo);
 
 alter table coches
 add constraint coches_marcas_FK foreign key(idmarca) references marcas(idmarca);
@@ -186,13 +199,26 @@ alter table coches
 add constraint coches_modelos_FK foreign key(idmodelo) references modelos(idmodelo);
 
 alter table coches
-add constraint coches_matricula_num_FK foreign key(matricula_num) references matriculas(matricula_num);
+add constraint coches_matricula_num_FK foreign key(matricula_num) references matricula(matricula_num);
+
+alter table coches
+add constraint coches_color_FK foreign key(color) references colores(color);
 
 alter table coches
 add constraint coches_kilometraje_FK foreign key(kilometraje) references kilometraje_total(kilometraje);
 
 alter table coches
-add constraint coches_color_FK foreign key(color) references colores(color);
+add constraint coches_aseguradoras_FK foreign key(id_aseguradoras) references aseguradoras(id_aseguradoras);
+
+alter table coches
+add constraint coches_polizas_FK foreign key(num_poliza) references polizas(num_poliza);
+
+alter table coches
+add constraint coches_revisiones_FK foreign key(num_revision) references revisiones(num_revision);
+
+alter table coches
+add constraint coches_kilometros_revision_FK foreign key(kilometros_revision) references kilometros_revisiones(kilometros_revision);
+
 
 --DML
 
@@ -206,85 +232,85 @@ insert into examen.grupo_empresarial  (idgrupo, name, comentario)
 values ('Nissan group', 'Nissan group','');
 
 insert into examen.grupo_empresarial  (idgrupo, name, comentario)
-values ('BMW group', 'BMW group','');
+values ('BMW Group', 'BMW group','');
 
 -- Datos Marcas
 
 insert into examen.marcas  (idmarca, name, comentario)
-values ('0001', 'Renault','');
+values ('Renault', 'Renault','');
 
 insert into examen.marcas  (idmarca, name, comentario)
-values ('0002', 'DACIA','');
+values ('Dacia', 'DACIA','');
 
 insert into examen.marcas  (idmarca, name, comentario)
-values ('0003', 'Nissan','');
+values ('Nissan', 'Nissan','');
 
 insert into examen.marcas  (idmarca, name, comentario)
-values ('0004', 'Mitsubisi','');
+values ('Mitsubisi', 'Mitsubisi','');
 
 insert into examen.marcas  (idmarca, name, comentario)
-values ('0005', 'Mini','');
+values ('Mini', 'Mini','');
 
 -- Datos modelos
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0001', 'Duster','');
+values ('Duster', 'Duster','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0002', 'Sandero','');
+values ('Sandero', 'Sandero','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0003', 'Megan','');
+values ('Megan', 'Megan','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0004', 'Clio','');
+values ('Clio', 'Clio','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0005', 'Juke','');
+values ('Juke', 'Juke','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0006', 'Qasqhai','');
+values ('Qasqhai', 'Qasqhai','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0007', 'Takai','');
+values ('Takai', 'Takai','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0009', 'Cabrio','');
+values ('Cabrio', 'Cabrio','');
 
 insert into examen.modelos (idmodelo, name, comentario)
-values ('0010', 'Clubman','');
+values ('Clubman', 'Clubman','');
 
 
 -- Datos fecha de compra
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2020-02-07', 'FEBRERO','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2019-10-07', '2019-10-07','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2022-01-01', '2022-01-01','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2019-02-07', '2019-02-07','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2021-03-02', '2021-03-02','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2022-02-03', '2022-02-03','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2018-06-07', '2018-06-07','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
-values ('2018-06-07', '2019-09-07','');
+insert into examen.fecha_compra  (fecha, name, comentario)
+values ('2018-01-07', '2019-09-07','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2018-02-09', '2018-02-09','');
 
-insert into examen.fecha_compra  (idfecha, name, comentario)
+insert into examen.fecha_compra  (fecha, name, comentario)
 values ('2027-02-07', '2027-02-07','');
 
 -- DATOS MATRICULAS
@@ -387,7 +413,7 @@ values ('B234567', 'Poliza 2','');
 insert into examen.polizas  (num_poliza , name, comentario)
 values ('C234567', 'Poliza 3','');
 
-insert into examen.polizas  (pnum_poliza , name, comentario)
+insert into examen.polizas  (num_poliza , name, comentario)
 values ('D234567', 'Poliza 4','');
 
 insert into examen.polizas  (num_poliza , name, comentario)
@@ -410,13 +436,13 @@ values ('P234567', 'Poliza 10','');
 
 -- datos nunmero revisiones
 
-insert into examen.revisiones (revisiones_num, name, comentario)
+insert into examen.revisiones (num_revision, name, comentario)
 values ('1', 'Primera','');
 
-insert into examen.revisiones (revisiones_num, name, comentario)
+insert into examen.revisiones (num_revision, name, comentario)
 values ('2', 'Segunda','');
 
-insert into examen.revisiones (revisiones_num, name, comentario)
+insert into examen.revisiones (num_revision, name, comentario)
 values ('3', 'Tercera','');
 
 -- datos kilometros revisiones
@@ -424,10 +450,10 @@ values ('3', 'Tercera','');
 insert into examen.kilometros_revisiones (kilometros_revision, name, comentario)
 values ('1000', 'Primera revision','');
 
-insert into examen.revisiones (revisiones_num, name, comentario)
+insert into examen.kilometros_revisiones (kilometros_revision, name, comentario)
 values ('2000', 'Segunda revision','');
 
-insert into examen.revisiones (revisiones_num, name, comentario)
+insert into examen.kilometros_revisiones (kilometros_revision, name, comentario)
 values ('5000', 'Tercera revision','');
 
 
@@ -446,27 +472,33 @@ values ('2022-02-07', 'Primera revision','');
 -- Datos importe revision
 
 insert into examen.importe_revisiones  (precio_revision, name, comentario)
-values ('500€', 'Primera revision','');
+values ('500', 'Primera revision','');
 
 insert into examen.importe_revisiones  (precio_revision, name, comentario)
-values ('1000€', 'Segunda revision','');
+values ('1000', 'Segunda revision','');
 
 insert into examen.importe_revisiones  (precio_revision, name, comentario)
-values ('1500€', 'Tercera revision','');
+values ('1500', 'Tercera revision','');
 
 
 --- Creamos los coches
 
-insert into examen.coches (idcoche, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, revisiones, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
-values ('0001', 'Renault Group','Renault', 'Megan','EU2891', 'VERDE', '2000', 'AXA', 'A234567', '1','1000','2018-02-07', '2018-02-07','500€','');
+insert into examen.coches (idcoche, name , idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0001','', 'Renault Group','Renault', 'Megan','EU2891', 'VERDE', '2000', 'AXA', 'A234567', '1','1000','2018-02-07', '2018-02-07','500','');
 
-insert into examen.coches (idcoche, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, revisiones, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
-values ('0002', 'Renault Group','Dacia', 'Sandero','EU2822', 'AMARILLO', '1000', 'MAPFRE', 'B234567', '2','2000','2019-10-07', '2020-02-07','500€','');
+insert into examen.coches (idcoche, name, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0002','', 'Renault Group','Dacia', 'Sandero','EU2822', 'AMARILLO', '1000', 'MAPFRE', 'B234567', '2','2000','2019-10-07', '2020-02-07','1000','');
 
-insert into examen.coches (idcoche, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, revisiones, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
-values ('0003', 'BMW Group','Mini', 'Cabrio','EU6789', 'AZUL', '2000', 'AXA', 'C234567', '1','1000','2020-02-07', '2018-02-07','500€','');
+insert into examen.coches (idcoche, name, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0003','', 'BMW Group','Mini', 'Cabrio','EU6789', 'AZUL', '2000', 'AXA', 'C234567', '1','1000','2020-02-07', '2018-02-07','500','');
 
-insert into examen.coches (idcoche, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, revisiones, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
-values ('0004', 'Nissan group','Mitsubisi', 'Takai','EU1111', 'GRIS', '1000', 'MAPFRE', 'G234567', '3','5000','2018-06-07', '2022-02-07','1500€','');
+insert into examen.coches (idcoche, name, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0004','', 'Nissan group','Mitsubisi', 'Takai','EU1111', 'GRIS', '1000', 'MAPFRE', 'G234567', '3','5000','2018-06-07', '2022-02-07','1500','');
 
+insert into examen.coches (idcoche, name, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0005','', 'Nissan group','Nissan', 'Qasqhai','EU1678', 'ROJO', '4000', 'AXA', 'I234567', '1','2000','2018-02-09', '2022-02-07','500','');
 
+insert into examen.coches (idcoche, name, idgrupo, idmarca, idmodelo, matricula_num, color, kilometraje, id_aseguradoras, num_poliza, num_revision, kilometros_revision, fecha, fecha_revision, precio_revision, comentario)
+values ('0006','', 'BMW Group','Mini', 'Clubman','EU2222', 'AMARILLO', '80000', 'AXA', 'H234567', '3','2000','2022-02-03', '2022-02-07','1500','');
+
+select * from examen.coches 
